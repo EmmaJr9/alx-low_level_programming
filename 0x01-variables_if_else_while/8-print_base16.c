6bb2bae8a574e8459279acaec87d0ffcf04379b1
Obeng Emmanuel Junior <eobengjunior@gmail.com>
2022-09-10 06:42:01 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * You can only use `putchar`, and only 3 times.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+
+	for(c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
