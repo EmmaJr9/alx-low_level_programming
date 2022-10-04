8079fb365ead7f20db92f75ebc418f8916322fff
Obeng Emmanuel Junior <eobengjunior@gmail.com>
2022-10-04 02:37:57 -0700

    0x0A C

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main
+ * @argc: int type
+ * @argv: char type
+ * Return: prints file names
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0, y = 0;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+
+		printf("%d\n", x*y);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
